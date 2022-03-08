72577c1828d09856242c68fdf6b28528b628c759
Tareq Hassan <tareqhassan2014@gmail.com>
2022-03-08 14:14:19 +0600

    update ckeckout and product detail

--- src/pages/Checkout.tsx
+++ src/pages/Checkout.tsx
@@ -1,13 +1,77 @@
-import React from 'react';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
+import {
+    Button,
+    Container,
+    Divider,
+    Grid,
+    IconButton,
+    Typography,
+} from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {
+    clearCart,
+    removeFromCart,
+} from '../redux/actionCreator/cartActionCreator';
+import { AppState } from '../redux/store';
 
 const Checkout = () => {
+    const dispatch = useDispatch();
+    let navigate = useNavigate();
+    const cart = useSelector((state: AppState) => state.cart);
     return (
-        <div>
-            <h1>Checkout</h1>
-            <h1>Checkout</h1>
-            <h1>Checkout</h1>
-            <h1>Checkout</h1>
-        </div>
+        <Container sx={{ my: 3 }}>
+            <Typography variant="h5">Shopping Cart</Typography>
+            <Grid container>
+                {cart.map((product) => (
+                    <Grid item key={product._id}>
+                        <Grid container alignItems="center">
+                            <Grid item xs={1}>
+                                <img
+                                    src={product.img}
+                                    alt=""
+                                    style={{ width: '100%' }}
+                                />
+                            </Grid>
+                            <Grid item xs={10} sx={{ px: 5 }}>
+                                <Typography>{product.name}</Typography>
+                            </Grid>
+                            <Grid item xs={1}>
+                                <IconButton
+                                    aria-label="delete"
+                                    size="large"
+                                    onClick={() =>
+                                        dispatch(removeFromCart(product._id))
+                                    }
+                                >
+                                    <DeleteIcon
+                                        fontSize="inherit"
+                                        color="error"
+                                    />
+                                </IconButton>
+                            </Grid>
+                        </Grid>
+                        <Divider />
+                    </Grid>
+                ))}
+            </Grid>
+            <Grid container direction="row-reverse" sx={{ mt: 2, mb: 5 }}>
+                <Grid item>
+                    <Button
+                        variant="contained"
+                        endIcon={<ShoppingCartCheckoutIcon />}
+                        size="large"
+                        onClick={() => {
+                            dispatch(clearCart());
+                            navigate('/');
+                        }}
+                    >
+                        Check Out
+                    </Button>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,12 +1,93 @@
-import React from 'react';
+import { Button, Container, Grid, Rating, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import useAsync from '../hooks/useAsync';
+import { addToCart } from '../redux/actionCreator/cartActionCreator';
+import ProductService from '../services/ProductService';
 
 const ProductDetail = () => {
+    let { _id } = useParams();
+
+    const getProduct = useCallback(() => {
+        return ProductService.getProductBuyId(_id!);
+    }, [_id]);
+
+    const { data, isSuccess } = useAsync<IProduct>(getProduct);
+
+    const { name, img, features, price, category, star } = (data ||
+        {}) as IProduct;
+
+    const dispatch = useDispatch();
+
     return (
-        <div>
-            <h1>ProductDetail</h1>
-            <h1>ProductDetail</h1>
-            <h1>ProductDetail</h1>
-        </div>
+        <Container>
+            {isSuccess && (
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="center"
+                    alignItems="stretch"
+                    spacing={{ xs: 2, md: 3 }}
+                >
+                    <Grid item xs={12} sm={6} md={6}>
+                        <img src={img} alt={name} style={{ width: '100%' }} />
+                    </Grid>
+                    <Grid item xs={12} sm={6} md={6}>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {name}
+                        </Typography>
+                        <Typography variant="h5" color="primary">
+                            {category}
+                        </Typography>
+
+                        <ul>
+                            {features.map((feature, index) => (
+                                <Typography
+                                    key={index}
+                                    gutterBottom
+                                    variant="body2"
+                                    component="li"
+                                >
+                                    {feature.description} : {feature.value}
+                                </Typography>
+                            ))}
+                        </ul>
+
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                            }}
+                        >
+                            <Typography component="span">Rating :</Typography>
+                            <Rating value={star} size="large" />
+                            <Typography component="span">({star})</Typography>
+                        </Box>
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                my: 3,
+                            }}
+                        >
+                            <Button
+                                size="large"
+                                color="primary"
+                                onClick={() => dispatch(addToCart(data!))}
+                            >
+                                ADD TO CARD
+                            </Button>
+                            <Button size="large" color="primary">
+                                $ {price} BUY NOW
+                            </Button>
+                        </Box>
+                    </Grid>
+                </Grid>
+            )}
+        </Container>
     );
 };
 
